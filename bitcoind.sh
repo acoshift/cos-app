4d4098ac662a05311b4fb4d8b6754bde334da9a5
acoshift <acoshift@gmail.com>
2018-01-01 18:28:17 +0700

    update bitcoind node

--- bitcoind.sh
+++ bitcoind.sh
@@ -1,23 +1,19 @@
 # prepare disk
-gcloud compute disks create disk-bitcoind --size=250GB --type=pd-standard
+gcloud compute disks create disk-btc-1 --size=250GB --type=pd-ssd
 gcloud compute instances create prepare-disk-instance --machine-type f1-micro
-gcloud compute instances attach-disk prepare-disk-instance --disk disk-bitcoind
+gcloud compute instances attach-disk prepare-disk-instance --disk disk-btc-1
 gcloud compute ssh prepare-disk-instance -- 'sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb'
+gcloud compute instances detach-disk prepare-disk-instance --disk disk-btc-1
 gcloud compute instances delete prepare-disk-instance
 
 # start container
-gcloud beta compute instances create-with-container bitcoind \
-  --machine-type n1-highcpu-4 \
+gcloud beta compute instances create-with-container btc-1 \
+  --machine-type n1-standard-2 \
   --container-image kylemanna/bitcoind@sha256:62afc775a839720ce608039e2ba1a5861a2d92b20a21ea24a1e3b1abda805e7c \
   --container-mount-host-path mount-path=/bitcoin,host-path=/mnt/disks/data,mode=rw \
   --container-arg="-dbcache=2000" \
   --container-arg="-rpcallowip=::/0" \
   --container-arg="-par=4" \
-  --container-arg="-maxuploadtarget=200" \
   --metadata ^:^startup-script="mkdir -p /mnt/disks/data && mount -o discard,defaults /dev/sdb /mnt/disks/data" \
-  --disk "name=disk-bitcoind,device-name=disk-bitcoind,mode=rw,boot=no" \
-  --tags allow-bitcoind
-
-# setup firewall
-gcloud compute firewall-rules create allow-bitcoind \
-  --allow tcp:8333,8332 --target-tags allow-bitcoind
+  --disk "name=disk-btc-1,device-name=disk-btc-1,mode=rw,boot=no" \
+  --tags btc-node
