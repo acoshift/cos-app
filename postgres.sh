3846634da16790e7304a500ac970f192493fd0b2
acoshift <acoshift@gmail.com>
2018-03-15 00:42:44 +0700

    update postgres

--- postgres.sh
+++ postgres.sh
@@ -1,5 +1,5 @@
 # prepare disk
-gcloud compute disks create data-postgres-1 --size=50GB --type=pd-ssd
+gcloud compute disks create data-postgres-1 --size=10GB --type=pd-ssd
 gcloud compute instances create prepare-disk-instance --machine-type f1-micro
 gcloud compute instances attach-disk prepare-disk-instance --disk data-postgres-1
 gcloud compute ssh prepare-disk-instance -- 'sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb'
@@ -8,8 +8,8 @@ gcloud compute instances delete prepare-disk-instance
 
 # start container
 gcloud beta compute instances create-with-container postgres-1 \
-  --machine-type n1-standard-2 \
-  --container-image postgres:10.2 \
+  --machine-type n1-standard-1 \
+  --container-image postgres:10.3 \
   --container-mount-host-path mount-path=/var/lib/postgresql/data,host-path=/mnt/disks/data/data,mode=rw \
   --metadata ^:^startup-script="mkdir -p /mnt/disks/data && mount -o discard,defaults /dev/sdb /mnt/disks/data" \
   --disk "name=data-postgres-1,device-name=data-postgres-1,mode=rw,boot=no"
