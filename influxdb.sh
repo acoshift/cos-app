687cbe5f584c437aaaac07014fd4f6600da897cf
acoshift <acoshift@gmail.com>
2018-07-15 09:43:44 +0700

    influxdb: add disk

--- influxdb.sh
+++ influxdb.sh
@@ -1,6 +1,16 @@
+# prepare disk
+gcloud compute disks create data-influxdb --size=10GB --type=pd-standard
+gcloud compute instances create prepare-disk-instance --machine-type f1-micro
+gcloud compute instances attach-disk prepare-disk-instance --disk data-influxdb
+gcloud compute ssh prepare-disk-instance -- 'sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb'
+gcloud compute instances detach-disk prepare-disk-instance --disk data-influxdb
+gcloud compute instances delete prepare-disk-instance
+
+# start container
 gcloud beta compute instances create-with-container influxdb \
   --machine-type g1-small \
-  --boot-disk-size "10" \
-  --boot-disk-type "pd-standard" \
   --container-image influxdb:1.6.0 \
   --container-mount-host-path mount-path=/var/lib/influxdb,host-path=/mnt/stateful_partition/data,mode=rw
+  --container-mount-host-path mount-path=/var/lib/influxdb,host-path=/mnt/disks/data/data,mode=rw \
+  --metadata ^:^startup-script="mkdir -p /mnt/disks/data && mount -o discard,defaults /dev/sdb /mnt/disks/data" \
+  --disk "name=data-influxdb,device-name=data-influxdb,mode=rw,boot=no"
