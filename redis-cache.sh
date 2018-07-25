ac4a37fa8ff351c9269d082b4340bdab609b3603
acoshift <acoshift@gmail.com>
2018-07-25 10:30:01 +0700

    update redis machine

--- redis-cache.sh
+++ redis-cache.sh
@@ -1,11 +1,12 @@
 gcloud beta compute instances create-with-container redis \
-  --machine-type f1-micro \
+  --custom-cpu=1 \
+  --custom-memory=1 \
   --boot-disk-size "10" \
   --boot-disk-type "pd-standard" \
   --container-image redis:4.0.10 \
   --container-command "redis-server" \
   --container-arg "--databases 1" \
   --container-arg "--save ''" \
-  --container-arg "--maxmemory 60mb" \
+  --container-arg "--maxmemory 500mb" \
   --container-arg "--maxmemory-policy allkeys-lru" \
   --metadata ^:^startup-script="echo never > /sys/kernel/mm/transparent_hugepage/enabled && sysctl -w net.core.somaxconn=65535"
