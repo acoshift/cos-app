41267a601acecd4d8cbf24acfedb15ad7625aae8
acoshift <acoshift@gmail.com>
2018-09-23 13:29:13 +0700

    update redis version

--- redis-local.sh
+++ redis-local.sh
@@ -2,7 +2,7 @@ gcloud beta compute instances create-with-container redis \
   --machine-type g1-small \
   --boot-disk-size "10" \
   --boot-disk-type "pd-standard" \
-  --container-image redis:4.0.10 \
+  --container-image redis:4.0.11 \
   --container-mount-host-path mount-path=/data,host-path=/mnt/stateful_partition/data,mode=rw \
   --container-command "redis-server" \
   --container-arg "--databases 1" \
